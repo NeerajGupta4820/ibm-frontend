c5f5e49ec98080bbfa8f271ab0f1034e90e6f1de
Neeraj Gupta <105665359+NeerajGupta4820@users.noreply.github.com>
2024-07-26 11:28:43 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 import './App.css';
+import Header from './components/Header';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import "../style/header.css"
+
+const Header = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+
+  return (
+    <nav className="header">
+      <div className="logo">
+        <h1>LOGO</h1>
+      </div>
+      <div className="search-bar">
+        <form  className="search-form">
+          <input
+            type="text"
+            placeholder="Search for products ...."
+          />
+          <button type="submit">
+            <FaSearch />
+          </button>
+        </form>
+      </div>
+      <div className={`header-center ${isMobileMenuOpen ? 'open' : ''}`}>
+        <Link onClick={() => setIsMobileMenuOpen(false)} to={"/"}>
+          HOME
+        </Link>
+        <div className="dropdown" >
+          <button >
+            CATEGORIES
+          </button>
+            <div className="dropdown-menu">
+                <Link>
+                <option>tutor</option>
+                <option>tuitioncenter</option>
+                </Link>
+
+            </div>
+        </div>
+        <Link onClick={() => setIsMobileMenuOpen(false)} to={"/about"}>
+          ABOUT
+        </Link>
+        <Link onClick={() => setIsMobileMenuOpen(false)} to={"/contact"}>
+          CONTACT
+        </Link>
+      </div>
+
+      {/* <div className="header-right">
+        <Link onClick={() => setIsMobileMenuOpen(false)} to={"/cart"}>
+          <FaShoppingBag />
+        </Link>
+          <>
+            <button onClick={() => setIsOpen((prev) => !prev)}>
+              <FaUser />
+            </button>
+            <dialog open={isOpen} className={`user-menu ${isMobileMenuOpen ? 'mobile-menu-dialog' : ''}`}>
+              <div>
+                  <Link onClick={() => setIsOpen(false)} to="/admin/dashboard">
+                    Admin
+                  </Link>
+
+                <Link onClick={() => setIsOpen(false)} to="/orders">
+                  Orders
+                </Link>
+                <button>
+                  <FaSignOutAlt />
+                </button>
+              </div>
+            </dialog>
+          </>
+          <Link to={"/login"}>
+            <FaSignInAlt />
+          </Link>
+        <button className="mobile-menu-toggle" onClick={() => setIsMobileMenuOpen((prev) => !prev)}>
+          <FaBars />
+        </button>
+      </div> */}
+    </nav>
+  );
+};
+
+export default Header;
