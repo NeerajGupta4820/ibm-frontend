2a995782d54e85c12934f01be15aa2af0a7d5fa9
Neeraj Gupta <105665359+NeerajGupta4820@users.noreply.github.com>
2024-07-30 18:28:58 +0530

    changes done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { FaFacebookF, FaInstagram, FaPinterestP, FaTwitter, FaYoutube } from 'react-icons/fa';
-import { SiMastercard, SiPaypal, SiVisa } from 'react-icons/si';
 import logo from "../assets/logo/logo.png";
 import '../style/footer.css';
+import img1 from "../assets/pay/app.jpg"
+import img2 from "../assets/pay/pay.png"
+import img3 from "../assets/pay/play.jpg"
+
 
 const Footer = () => {
   return (
@@ -27,33 +30,31 @@ const Footer = () => {
         </div>
         <div className="col">
         <h4>About</h4>
-        <a href="#">About us</a>
+        <a href="/about">About us</a>
         <a href="#">Delivery Information</a>
         <a href="#">Privacy Policy</a>
         <a href="#">Terms & condition</a>
-        <a href="#">Contact us</a>
+        <a href="/contact">Contact us</a>
         </div>
         <div className="col">
         <h4>My Account</h4>
-        <a href="#">Sign In</a>
+        <a href="/login">Sign In</a>
         <a href="#">View cart</a>
         <a href="#">My wishlist</a>
         <a href="#">Track my order</a>
-        <a href="#">Help</a>
+        <a href="/about">Help</a>
         </div>
         <div className="col" id="install">
         <h4>Install app</h4>
         <p>From App Store or Google Play</p>
         <div className="row">
-          <img src="../assets/app.png" alt="App Store" />
-          <img src="../assets/play.png" alt="Google Play" />
+          <img src={img1} alt="App Store" />
+          <img src={img3} alt="Google Play" />
         </div>
         <p>Secured payment gateways</p>
         <div className="payment-icons">
-         <SiVisa className="visa" />
-          <SiMastercard className="mastercard" />
-          <SiPaypal className="paypal" />
-      </div>
+          <img src={img2} alt="Payment" />
+        </div>
         </div>
       </div>
       <div className="copyright">
