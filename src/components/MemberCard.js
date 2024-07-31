863ed2d697bfbfe52b362f6bf64c49285abdf293
Ganesh-2205 <104447530+Ganesh-2205@users.noreply.github.com>
2024-07-31 20:43:46 +0530

    done

---
+++ src/components/MemberCard.js
@@ -0,0 +1,15 @@
+// MemberCard.jsx
+import React from 'react';
+import '../style/memberCard.css'; 
+const MemberCard = ({ image, name, role, description }) => {
+  return (
+    <div className="member-card">
+      <img src={image} alt={name} className="member-image" />
+      <h3 className="member-name">{name}</h3>
+      <p className="member-role">{role}</p>
+      <p className="member-description">{description}</p>
+    </div>
+  );
+};
+
+export default MemberCard;
